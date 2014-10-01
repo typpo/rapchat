6c3e4c68a3dfda183a4c9b13cdf85b844ddef86b
Ian <typppo@gmail.com>
2014-09-30 23:12:37 -0700

    add stickers

---
+++ stickers.js
@@ -0,0 +1,1092 @@
+window.STICKERS = [
+{
+name: "2 chainz",
+slug: "twochainz",
+audio: "audio/2chainz_4.ogg"
+},
+{
+name: "2 chainz",
+slug: "twochainz",
+audio: "audio/2chainz_yeah2.ogg"
+},
+{
+name: "2pac",
+slug: "tupac",
+audio: "audio/2pac_4.ogg"
+},
+{
+name: "2pac",
+slug: "tupac",
+audio: "audio/2pac_5.ogg"
+},
+{
+name: "2pac",
+slug: "tupac",
+audio: "audio/2pac_6.ogg"
+},
+{
+name: "50 Cent",
+slug: "fiftycent",
+audio: "audio/50_5.ogg"
+},
+{
+name: "50 Cent",
+slug: "fiftycent",
+audio: "audio/50_8.ogg"
+},
+{
+name: "50 Cent",
+slug: "fiftycent",
+audio: "audio/50_11.ogg"
+},
+{
+name: "50 Cent",
+slug: "fiftycent",
+audio: "audio/50cent_2.ogg"
+},
+{
+name: "Action Bronson",
+slug: "actionbronson",
+audio: "audio/action_bronsolino.ogg"
+},
+{
+name: "Action Bronson",
+slug: "actionbronson",
+audio: "audio/action_yeah.ogg"
+},
+{
+name: "Action Bronson",
+slug: "actionbronson",
+audio: "audio/action_yo.ogg"
+},
+{
+name: "Akon",
+slug: "akon",
+audio: "audio/akon_1.ogg"
+},
+{
+name: "Beanie Sigel",
+slug: "beaniesigel",
+audio: "audio/beanie_mac.ogg"
+},
+{
+name: "Big Boi",
+slug: "bigboi",
+audio: "audio/bigboi_1.ogg"
+},
+{
+name: "Big L",
+slug: "bigl",
+audio: "audio/bigl_3.ogg"
+},
+{
+name: "Big L",
+slug: "bigl",
+audio: "audio/bigl_4.ogg"
+},
+{
+name: "Big L",
+slug: "bigl",
+audio: "audio/bigl_5.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_boi2.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_doit.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_holdup2.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_ohgod.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_okay.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_stop.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_whoa.ogg"
+},
+{
+name: "Big Sean",
+slug: "bigsean",
+audio: "audio/bigsean_whoathere.ogg"
+},
+{
+name: "Birdman",
+slug: "birdman",
+audio: "audio/birdman_4.ogg"
+},
+{
+name: "Birdman",
+slug: "birdman",
+audio: "audio/birdman_10.ogg"
+},
+{
+name: "Birdman",
+slug: "birdman",
+audio: "audio/birdman_16.ogg"
+},
+{
+name: "Birdman",
+slug: "birdman",
+audio: "audio/birdman_1.ogg"
+},
+{
+name: "Birdman",
+slug: "birdman",
+audio: "audio/birdman_6.ogg"
+},
+{
+name: "Bow Wow",
+slug: "bowwow",
+audio: "audio/bowwow_yeah.ogg"
+},
+{
+name: "Bun B",
+slug: "bunb",
+audio: "audio/bunb_ugk4life.ogg"
+},
+{
+name: "Busta Rhymes",
+slug: "busta",
+audio: "audio/busta_6.ogg"
+},
+{
+name: "Busta Rhymes",
+slug: "busta",
+audio: "audio/busta_1.ogg"
+},
+{
+name: "Busta Rhymes",
+slug: "busta",
+audio: "audio/busta_2.ogg"
+},
+{
+name: "Busta Rhymes",
+slug: "busta",
+audio: "audio/busta_5.ogg"
+},
+{
+name: "Cam'Ron",
+slug: "camron",
+audio: "audio/camron_1.ogg"
+},
+{
+name: "Cam'Ron",
+slug: "camron",
+audio: "audio/camron_2.ogg"
+},
+{
+name: "Chief Keef",
+slug: "chiefkeef",
+audio: "audio/keef_bang.ogg"
+},
+{
+name: "Chief Keef",
+slug: "chiefkeef",
+audio: "audio/keef_catchup.ogg"
+},
+{
+name: "Chingy",
+slug: "chingy",
+audio: "audio/chingy_1.ogg"
+},
+{
+name: "Curren$y",
+slug: "currensy",
+audio: "audio/currensy_1.ogg"
+},
+{
+name: "Da Brat",
+slug: "dabrat",
+audio: "audio/dabrat_comeon.ogg"
+},
+{
+name: "Da Brat",
+slug: "dabrat",
+audio: "audio/dabrat_lookout.ogg"
+},
+{
+name: "Da Brat",
+slug: "dabrat",
+audio: "audio/dabrat_oh.ogg"
+},
+{
+name: "Danny Brown",
+slug: "dannybrown",
+audio: "audio/danny_stop.ogg"
+},
+{
+name: "Danny Brown",
+slug: "dannybrown",
+audio: "audio/danny_yeah.ogg"
+},
+{
+name: "Danny Brown",
+slug: "dannybrown",
+audio: "audio/dannybrown_laugh2.ogg"
+},
+{
+name: "David Banner",
+slug: "davidbanner",
+audio: "audio/davidbanner_5.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_1.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_3.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_4.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_5.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_6.ogg"
+},
+{
+name: "Diddy",
+slug: "diddy",
+audio: "audio/diddy_7.ogg"
+},
+{
+name: "Dizzee Rascal",
+slug: "dizzee",
+audio: "audio/dizzee_1.ogg"
+},
+{
+name: "DJ Khaled",
+slug: "khaled",
+audio: "audio/djkhaled_2.ogg"
+},
+{
+name: "DJ Khaled",
+slug: "khaled",
+audio: "audio/djkhaled_3.ogg"
+},
+{
+name: "DJ Paul",
+slug: "djpaul",
+audio: "audio/djpaul_2.ogg"
+},
+{
+name: "DJ Paul",
+slug: "djpaul",
+audio: "audio/djpaul_3.ogg"
+},
+{
+name: "DJ Paul",
+slug: "djpaul",
+audio: "audio/djpaul_9.ogg"
+},
+{
+name: "DMX",
+slug: "dmx",
+audio: "audio/dmx_1.ogg"
+},
+{
+name: "DMX",
+slug: "dmx",
+audio: "audio/dmx_3.ogg"
+},
+{
+name: "DMX",
+slug: "dmx",
+audio: "audio/dmx_6.ogg"
+},
+{
+name: "DMX",
+slug: "dmx",
+audio: "audio/dmx_7.ogg"
+},
+{
+name: "Drake",
+slug: "drake",
+audio: "audio/drake_2.ogg"
+},
+{
+name: "Drake",
+slug: "drake",
+audio: "audio/drake_3.ogg"
+},
+{
+name: "Drake",
+slug: "drake",
+audio: "audio/drake_4.ogg"
+},
+{
+name: "Drake",
+slug: "drake",
+audio: "audio/drake_5.ogg"
+},
+{
+name: "Drumma Boy",
+slug: "drummaboy",
+audio: "audio/drummaboy_1.ogg"
+},
+{
+name: "E-40",
+slug: "e40",
+audio: "audio/e40_1.ogg"
+},
+{
+name: "E-40",
+slug: "e40",
+audio: "audio/e40_2.ogg"
+},
+{
+name: "Eazy-E",
+slug: "eazye",
+audio: "audio/eazye_1.ogg"
+},
+{
+name: "Eminem",
+slug: "eminem",
+audio: "audio/eminem_3.ogg"
+},
+{
+name: "Eminem",
+slug: "eminem",
+audio: "audio/eminem_4.ogg"
+},
+{
+name: "Fat Joe",
+slug: "fatjoe",
+audio: "audio/fatjoe_1.ogg"
+},
+{
+name: "Fat Joe",
+slug: "fatjoe",
+audio: "audio/fatjoe_9.ogg"
+},
+{
+name: "Fat Joe",
+slug: "fatjoe",
+audio: "audio/fatjoe_5.ogg"
+},
+{
+name: "Flavor Flav",
+slug: "flava",
+audio: "audio/flava_1.ogg"
+},
+{
+name: "Freeway",
+slug: "freeway",
+audio: "audio/freeway_1.ogg"
+},
+{
+name: "French Montana",
+slug: "frenchie",
+audio: "audio/french_1.ogg"
+},
+{
+name: "Ghostface Killah",
+slug: "ghostface",
+audio: "audio/ghostface_yo.ogg"
+},
+{
+name: "Grandmaster Flash",
+slug: "grandmaster",
+audio: "audio/grandmaster_1.ogg"
+},
+{
+name: "Gucci Mane",
+slug: "gucci",
+audio: "audio/gucci_1.ogg"
+},
+{
+name: "Gucci Mane",
+slug: "gucci",
+audio: "audio/gucci_4.ogg"
+},
+{
+name: "Gucci Mane",
+slug: "gucci",
+audio: "audio/gucci_14.ogg"
+},
+{
+name: "Gucci Mane",
+slug: "gucci",
+audio: "audio/gucci_8.ogg"
+},
+{
+name: "Gucci Mane",
+slug: "gucci",
+audio: "audio/gucci_9.ogg"
+},
+{
+name: "Hurricane Chris",
+slug: "hurricane",
+audio: "audio/hurricanechris_1.ogg"
+},
+{
+name: "Ice Cube",
+slug: "icecube",
+audio: "audio/icecube_1.ogg"
+},
+{
+name: "Inspctah Deck",
+slug: "inspectahdeck",
+audio: "audio/inspectahdeck_killahill.ogg"
+},
+{
+name: "Jadakiss",
+slug: "jadakiss",
+audio: "audio/jadakiss_3.ogg"
+},
+{
+name: "Ja Rule",
+slug: "jarule",
+audio: "audio/jarule_1.ogg"
+},
+{
+name: "Ja Rule",
+slug: "jarule",
+audio: "audio/jarule_2.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_7.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_9.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_1.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz5.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz7.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz8.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_itsthero.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_itsyoboy.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_jiggaman.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_woo.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_yessir.ogg"
+},
+{
+name: "Jay-Z",
+slug: "jayz",
+audio: "audio/jayz_young.ogg"
+},
+{
+name: "Jazzy Phae",
+slug: "jazzypha",
+audio: "audio/jazzypha_1.ogg"
+},
+{
+name: "Jermaine Dupri",
+slug: "jd",
+audio: "audio/jermaine_unh.ogg"
+},
+{
+name: "Jim Jones",
+slug: "jimjones",
+audio: "audio/jones_8.ogg"
+},
+{
+name: "Jim Jones",
+slug: "jimjones",
+audio: "audio/jones_14.ogg"
+},
+{
+name: "Juelz Santana",
+slug: "juelz",
+audio: "audio/juelz_2.ogg"
+},
+{
+name: "Juicy J",
+slug: "juicyj",
+audio: "audio/juicyj_1.ogg"
+},
+{
+name: "Juicy J",
+slug: "juicyj",
+audio: "audio/juicyj_8.ogg"
+},
+{
+name: "Juicy J",
+slug: "juicyj",
+audio: "audio/juicyj_9.ogg"
+},
+{
+name: "Juicy J",
+slug: "juicyj",
+audio: "audio/juicyj_10.ogg"
+},
+{
+name: "Juicy J",
+slug: "juicyj",
+audio: "audio/juicyj_7.ogg"
+},
+{
+name: "Kanye West",
+slug: "kanye",
+audio: "audio/kanye_1.ogg"
+},
+{
+name: "Killer Mike",
+slug: "killermike",
+audio: "audio/killermike_2.ogg"
+},
+{
+name: "Killer Mike",
+slug: "killermike",
+audio: "audio/killermike_3.ogg"
+},
+{
+name: "KRS One",
+slug: "krsone",
+audio: "audio/krsone_1.ogg"
+},
+{
+name: "Lil B",
+slug: "lilb",
+audio: "audio/lilb_1.ogg"
+},
+{
+name: "Lil B",
+slug: "lilb",
+audio: "audio/lilb_2.ogg"
+},
+{
+name: "Lil Jon",
+slug: "liljon",
+audio: "audio/liljon_2.ogg"
+},
+{
+name: "Lil Jon",
+slug: "liljon",
+audio: "audio/liljon_3.ogg"
+},
+{
+name: "Lil Jon",
+slug: "liljon",
+audio: "audio/liljon_8.ogg"
+},
+{
+name: "Lil Jon",
+slug: "liljon",
+audio: "audio/liljon_4.ogg"
+},
+{
+name: "Lil Scrappy",
+slug: "lilscrappy",
+audio: "audio/lilscrappy_1.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_14.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_22.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_29.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_4.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_16.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_17.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_25.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_30.ogg"
+},
+{
+name: "Lil Wayne",
+slug: "weezy",
+audio: "audio/weezy_31.ogg"
+},
+{
+name: "Lloyd Banks",
+slug: "lloydbanks",
+audio: "audio/banks_unh.ogg"
+},
+{
+name: "Lloyd Banks",
+slug: "lloydbanks",
+audio: "audio/banks_yeah.ogg"
+},
+{
+name: "Ludacris",
+slug: "ludacris",
+audio: "audio/ludacris_2.ogg"
+},
+{
+name: "Ludacris",
+slug: "ludacris",
+audio: "audio/ludacris_woo.ogg"
+},
+{
+name: "Mannie Fresh",
+slug: "mannie",
+audio: "audio/mannie_1.ogg"
+},
+{
+name: "Mannie Fresh",
+slug: "mannie",
+audio: "audio/mannie_2.ogg"
+},
+{
+name: "MC Eiht",
+slug: "mceight",
+audio: "audio/mceiht_1.ogg"
+},
+{
+name: "MC Hammer",
+slug: "mchammer",
+audio: "audio/mchammer_1.ogg"
+},
+{
+name: "Method Man",
+slug: "methodman",
+audio: "audio/methodman_1.ogg"
+},
+{
+name: "Method Man",
+slug: "methodman",
+audio: "audio/methodman_tical.ogg"
+},
+{
+name: "Method Man",
+slug: "methodman",
+audio: "audio/methodman_yo.ogg"
+},
+{
+name: "Mike Jones",
+slug: "mikejones",
+audio: "audio/mikejones_2.ogg"
+},
+{
+name: "Mike Jones",
+slug: "mikejones",
+audio: "audio/mikejones_iceage.ogg"
+},
+{
+name: "Mike Jones",
+slug: "mikejones",
+audio: "audio/mikejones_jyeah.ogg"
+},
+{
+name: "M.O.P.",
+slug: "mop",
+audio: "audio/mop_1.ogg"
+},
+{
+name: "M.O.P.",
+slug: "mop",
+audio: "audio/mop_2.ogg"
+},
+{
+name: "Nas",
+slug: "nas",
+audio: "audio/nas_1.ogg"
+},
+{
+name: "Nas",
+slug: "nas",
+audio: "audio/nas_3.ogg"
+},
+{
+name: "Nas",
+slug: "nas",
+audio: "audio/nas_6.ogg"
+},
+{
+name: "Nate Dogg",
+slug: "natedogg",
+audio: "audio/natedogg_1.ogg"
+},
+{
+name: "N.O.R.E.",
+slug: "noreaga",
+audio: "audio/nore_1.ogg"
+},
+{
+name: "The Notorious B.I.G.",
+slug: "biggie",
+audio: "audio/biggie_1.ogg"
+},
+{
+name: "The Notorious B.I.G.",
+slug: "biggie",
+audio: "audio/biggie_2.ogg"
+},
+{
+name: "OJ Da Juiceman",
+slug: "oj",
+audio: "audio/oj_4.ogg"
+},
+{
+name: "Ol Dirty Bastard",
+slug: "odb",
+audio: "audio/odb_shimmy.ogg"
+},
+{
+name: "Ol Dirty Bastard",
+slug: "odb",
+audio: "audio/odb_yo.ogg"
+},
+{
+name: "Pharrell",
+slug: "pharrell",
+audio: "audio/pharrell_1.ogg"
+},
+{
+name: "Pharrell",
+slug: "pharrell",
+audio: "audio/pharrell_2.ogg"
+},
+{
+name: "Pill",
+slug: "pill",
+audio: "audio/pill_1.ogg"
+},
+{
+name: "Pimp C",
+slug: "pimpc",
+audio: "audio/pimpc_4.ogg"
+},
+{
+name: "Pimp C",
+slug: "pimpc",
+audio: "audio/pimpc_1.ogg"
+},
+{
+name: "Pimp C",
+slug: "pimpc",
+audio: "audio/pimpc_sweetjones3.ogg"
+},
+{
+name: "Pitbull",
+slug: "pitbull",
+audio: "audio/pitbull_1.ogg"
+},
+{
+name: "Pitbull",
+slug: "pitbull",
+audio: "audio/pitbull_2.ogg"
+},
+{
+name: "Pitbull",
+slug: "pitbull",
+audio: "audio/pitbull_3.ogg"
+},
+{
+name: "Pitbull",
+slug: "pitbull",
+audio: "audio/pitbull_6.ogg"
+},
+{
+name: "Project Pat",
+slug: "projectpat",
+audio: "audio/projectpat_1.ogg"
+},
+{
+name: "Pusha T",
+slug: "pushat",
+audio: "audio/pushat_1.ogg"
+},
+{
+name: "Pusha T",
+slug: "pushat",
+audio: "audio/pushat_haha.ogg"
+},
+{
+name: "Pusha T",
+slug: "pushat",
+audio: "audio/pushat_unh.ogg"
+},
+{
+name: "Raekwon",
+slug: "raekwon",
+audio: "audio/raekwon_yo.ogg"
+},
+{
+name: "Redman",
+slug: "redman",
+audio: "audio/redman_heyo.ogg"
+},
+{
+name: "Rick Ross",
+slug: "ross",
+audio: "audio/ross_1.ogg"
+},
+{
+name: "Rick Ross",
+slug: "ross",
+audio: "audio/ross_2.ogg"
+},
+{
+name: "Rick Ross",
+slug: "ross",
+audio: "audio/ross_4.ogg"
+},
+{
+name: "Rick Ross",
+slug: "ross",
+audio: "audio/ross_woo.ogg"
+},
+{
+name: "Sean Price",
+slug: "seanprice",
+audio: "audio/seanprice_1.ogg"
+},
+{
+name: "Slim Thug",
+slug: "thugga",
+audio: "audio/thugga_2.ogg"
+},
+{
+name: "Slim Thug",
+slug: "thugga",
+audio: "audio/thugga_3.ogg"
+},
+{
+name: "Snoop Dogg",
+slug: "snoop",
+audio: "audio/snoop_5.ogg"
+},
+{
+name: "Snoop Dogg",
+slug: "snoop",
+audio: "audio/snoop_4.ogg"
+},
+{
+name: "Snoop Dogg",
+slug: "snoop",
+audio: "audio/snoop_1.ogg"
+},
+{
+name: "Soulja Boy",
+slug: "soulja",
+audio: "audio/soulja_2.ogg"
+},
+{
+name: "Soulja Boy",
+slug: "soulja",
+audio: "audio/soulja_4.ogg"
+},
+{
+name: "Soulja Boy",
+slug: "soulja",
+audio: "audio/soulja_5.ogg"
+},
+{
+name: "Swizz Beatz",
+slug: "swizz",
+audio: "audio/swizz_1.ogg"
+},
+{
+name: "Swizz Beatz",
+slug: "swizz",
+audio: "audio/swizz_goddamit.ogg"
+},
+{
+name: "T.I.",
+slug: "ti",
+audio: "audio/ti_5.ogg"
+},
+{
+name: "T.I.",
+slug: "ti",
+audio: "audio/ti_2.ogg"
+},
+{
+name: "T.I.",
+slug: "ti",
+audio: "audio/ti_3.ogg"
+},
+{
+name: "T.I.",
+slug: "ti",
+audio: "audio/ti_22.ogg"
+},
+{
+name: "T.I.",
+slug: "ti",
+audio: "audio/ti_32.ogg"
+},
+{
+name: "Too $hort",
+slug: "tooshort",
+audio: "audio/tooshort_1.ogg"
+},
+{
+name: "T-Pain",
+slug: "tpain",
+audio: "audio/tpain_2.ogg"
+},
+{
+name: "T-Pain",
+slug: "tpain",
+audio: "audio/tpain1.ogg"
+},
+{
+name: "Trey Songz",
+slug: "treysongz",
+audio: "audio/treysongz_4.ogg"
+},
+{
+name: "Trick Daddy",
+slug: "trickdaddy",
+audio: "audio/trick_1.ogg"
+},
+{
+name: "Trick Daddy",
+slug: "trickdaddy",
+audio: "audio/trick_2.ogg"
+},
+{
+name: "Trick Daddy",
+slug: "trickdaddy",
+audio: "audio/trick_4.ogg"
+},
+{
+name: "Tyga",
+slug: "tyga",
+audio: "audio/tyga_unh.ogg"
+},
+{
+name: "Vado",
+slug: "vado",
+audio: "audio/vado_1.ogg"
+},
+{
+name: "Waka Flocka Flame",
+slug: "waka",
+audio: "audio/waka_1.ogg"
+},
+{
+name: "Waka Flocka Flame",
+slug: "waka",
+audio: "audio/waka_8.ogg"
+},
+{
+name: "Will Smith",
+slug: "willsmith",
+audio: "audio/willsmith_1.ogg"
+},
+{
+name: "Will Smith",
+slug: "willsmith",
+audio: "audio/willsmith_2.ogg"
+},
+{
+name: "Will Smith",
+slug: "willsmith",
+audio: "audio/willsmith_3.ogg"
+},
+{
+name: "Wiz Khalifa",
+slug: "wiz",
+audio: "audio/wiz_1.ogg"
+},
+{
+name: "Wiz Khalifa",
+slug: "wiz",
+audio: "audio/wiz_unh.ogg"
+},
+{
+name: "Young Jeezy",
+slug: "jeezy",
+audio: "audio/jeezy_10.ogg"
+},
+{
+name: "Young Jeezy",
+slug: "jeezy",
+audio: "audio/jeezy_11.ogg"
+},
+{
+name: "Young Jeezy",
+slug: "jeezy",
+audio: "audio/jeezy_1.ogg"
+}
+]
